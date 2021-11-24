04b06bc16d39f4f9d43f8d8f1b6e702d42401b98
YK911 <jaroslavkositsa@gmail.com>
2021-11-24 13:52:09 +0200

    add form styles

---
+++ js/input-value.js
@@ -0,0 +1,11 @@
+(() => {
+  const form = document.querySelector(".js-form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    new FormData(e.currentTarget).forEach((value, name) =>
+      console.log(`${name} : ${value}`)
+    );
+    e.currentTarget.reset();
+  });
+})();
